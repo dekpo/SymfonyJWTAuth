993c9dfe4d5e06fc4932e232a7d3dc898153be89
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2023-07-01 00:43:39 +0200

    Added cats to login cnotroller

--- src/Controller/LoginFormController.php
+++ src/Controller/LoginFormController.php
@@ -6,9 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Repository\CategoryRepository;
 
 class LoginFormController extends AbstractController
-{
+{   
+    private $allCategories;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        $this->allCategories = $categoryRepository->findBy(array(), array('name' => 'ASC'));
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -21,7 +29,7 @@ class LoginFormController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error, 'categories' => $this->allCategories]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
